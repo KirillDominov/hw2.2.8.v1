b31e8acbfe586d2e017e9ae0b683e2dd5a175aa2
KirillDominov <spotik321@gmail.com>
2024-12-13 21:35:22 +0500

    Init

---
+++ src/main/java/pro/sky/skypro/hw228/SkyproSpringbootApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skypro.hw228;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skypro/hw228/SkyproSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skypro.hw228;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
